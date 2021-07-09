94b16ffd81eb2bfb339a076d0cb9ca3359f784c0
Jeong <jeongam5785@gmail.com>
2021-07-09 11:15:57 +0900

    210709

--- marker_pose/src/go_stop.cpp
+++ marker_pose/src/go_stop.cpp
@@ -15,7 +15,7 @@ void msgCallback(const visualization_msgs::Marker::ConstPtr&msg)
     pub = nh.advertise<geometry_msgs::Twist>("/cmd_vel",100);
     //pub = nh.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel",100);
     geometry_msgs::Twist pub_msg;
-    // ros::Rate r(1);
+    ros::Rate r(1);
 
     int id = msg->id;
     double point_x = msg->pose.position.x;
@@ -25,36 +25,12 @@ void msgCallback(const visualization_msgs::Marker::ConstPtr&msg)
     double ori_y = msg->pose.orientation.y;
     double ori_z = msg->pose.orientation.z;
     double ori_w = msg->pose.orientation.w;
-    double roll, pitch, yaw;
-    
-    if (sleep_cnt > 10)
-    {
-        sleep_cnt = 0;
-        id = 0;
-        pub_msg.linear.x = 0.0;
-        pub.publish(pub_msg);
-        ROS_INFO("ssssssssssssssssssssssssss");        
-    }
-
-    double sinr_cosp = 2 * (ori_w * ori_x + ori_y * ori_z);
-    double cosr_cosp = 1 - 2 * (ori_x * ori_x + ori_y * ori_y);
-    roll = atan2(sinr_cosp, cosr_cosp);
-
-    double sinp = 2 * (ori_w * ori_y - ori_z * ori_x);
-    if (abs(sinp) >= 1)
-        pitch = copysign(M_PI / 2, sinp); 
-    else
-        pitch = asin(sinp);
-
-    double siny_cosp = 2 * (ori_w * ori_z + ori_x * ori_y);
-    double cosy_cosp = 1 - 2 * (ori_y * ori_y + ori_z * ori_z);
-    yaw = atan2(siny_cosp, cosy_cosp);
 
     ROS_INFO("id = %d", id);
     ROS_INFO("point_x = %f", point_x);
     ROS_INFO("distance = %f", point_z);
 
-        if (id == 2)
+        if (id == 3)
         {
             if (point_x > 0.1)
             {
@@ -80,11 +56,18 @@ void msgCallback(const visualization_msgs::Marker::ConstPtr&msg)
                 else
                 {
                     ROS_INFO("stop");
-                    ros::Duration(1.5).sleep();
+                    ros::Duration(2.0).sleep();
                     pub_msg.linear.x = 0;
                     pub_msg.angular.z = 0;
                     pub.publish(pub_msg);
+                    pub_msg.angular.z = -1.5707963268/4.0;
+                    pub.publish(pub_msg);
                     sleep_cnt++;
+                    if (sleep_cnt > 2)
+                    {
+                        pub_msg.angular.z = 0;
+                        pub.publish(pub_msg);
+                    }
                 }
                     // if()
                     // {
@@ -100,12 +83,12 @@ void msgCallback(const visualization_msgs::Marker::ConstPtr&msg)
                 // }
             }
         }
-        // else
-        // {
-        //     ROS_INFO("Go foward");
-        //     pub_msg.linear.x = 0.1;
-        //     pub.publish(pub_msg);
-        // }
+        else
+        {
+            ROS_INFO("Go foward");
+            pub_msg.linear.x = 0.1;
+            pub.publish(pub_msg);
+        }
     // r.sleep();
 }
 
